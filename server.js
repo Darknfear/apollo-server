7bcb69861e6fffd8c9d37871553b04ff4acd1d52
hieu.nguyenvan <hieu.nguyenvan@powergateaustralia.com>
2022-09-08 15:50:11 +0700

    add healthy check

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const initialApp = require('./src/app');
 
 initialApp().then((app) => {
+    app.get('/healthy-check', (req, res) => {
+        return res.status(200).json({
+            success: true
+        })
+    })
     app.listen(3000, () => {
         console.log(`🚀 Server ready at http://localhost:3000`);
     });
