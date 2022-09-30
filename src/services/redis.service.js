9d8997461c837a199ab0c880db0c14b65aa493ac
hieu.nguyenvan <hieu.nguyenvan@powergateaustralia.com>
2022-09-30 17:24:58 +0700

    config redis

---
+++ src/services/redis.service.js
@@ -0,0 +1,30 @@
+const { createClient } = require('redis');
+
+const client = createClient({ url: 'http://localhost:6379'});
+
+const redisSetKey = async (key, value) => {
+  try {
+    const redisValue = await client.get(key);
+    if (redisValue) {
+      return false;
+    }
+    await client.set(key, value);
+    return true;
+  } catch (err) {
+    throw new Error(err.message);
+  } 
+}
+
+const redisGetKey = async (key) => {
+  return await client.get(key);
+}
+
+const redisDeleteKey = async (key) => {
+  return await client.del(key);
+}
+
+module.exports = {
+  redisSetKey,
+  redisGetKey,
+  redisDeleteKey
+}
