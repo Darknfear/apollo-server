47875a84d0647af93eba34e2415f75b4fa41ff25
hieu.nguyenvan <hieu.nguyenvan@powergateaustralia.com>
2022-10-06 17:39:22 +0700

    update config rabbitmq

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const connectionRabbitMQ = require('./shared/rabbitmq/rabbitmq.shared');
 const apolloServer = require('./apollo');
 
 
 const initialApp = async () => {
     const app = express();
     await apolloServer.start();
+    await connectionRabbitMQ();
     apolloServer.applyMiddleware({ app });
     return app;
 }
